0f917fcf92ebd6abeef317eaf91950e52046f36e
ambateman <tmbateman@hotmail.com>
2017-09-30 12:26:47 -0700

    Completed version of challenge 14 Simple calculator.

---
+++ SimpleCalculator/Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="SimpleCalculator.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body style="height: 260px">
+    <h2><em>Simple Calculator</em></h2>
+
+    <form id="form1" runat="server">
+        <div style="font-family: Arial, Helvetica, sans-serif; font-weight: normal">
+            First Value:
+            <asp:TextBox ID="firstNumber" runat="server"></asp:TextBox>
+            <br />
+            Second Value:
+            <asp:TextBox ID="secondNumber" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="Addition" runat="server" OnClick="Addition_Click" Text=" + " />
+            <asp:Button ID="Subtraction" runat="server" Text=" - " OnClick="Subtraction_Click" />
+            <asp:Button ID="Multiplication" runat="server" Text=" * " OnClick="Multiplication_Click" />
+            <asp:Button ID="Division" runat="server" Text=" / " OnClick="Division_Click" />
+            <br />
+            <br />
+            <asp:Label ID="Result" runat="server" BackColor="#3399FF" Font-Bold="True" Font-Size="X-Large"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ SimpleCalculator/Default.aspx.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SimpleCalculator
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Addition_Click(object sender, EventArgs e)
+        {
+            //check if the text values can be converted to number
+            //I opted for using decimal just in case.
+            //I also test for the presence of both numbers.
+            //This code can be largely reused for the other three operators.
+
+            if (CheckBoxValues())
+            {
+                decimal num1;
+                decimal num2;
+                decimal result;
+                bool num1Test = decimal.TryParse(firstNumber.Text, out num1);
+                bool num2test = decimal.TryParse(secondNumber.Text, out num2);
+                if(num1Test && num2test)
+                {
+                    result = num1 + num2;  //Here's where the operator is used.
+                    Result.Text = result.ToString();
+
+                }
+                else
+                {
+                    Result.Text = "Cannot do arithmetic without two numbers. Check inputs and try again.";
+                }
+ 
+            }
+        }
+
+
+
+        protected bool CheckBoxValues()
+        {
+
+            return true;
+        }
+
+        protected void Subtraction_Click(object sender, EventArgs e)
+        {
+            //check if the text values can be converted to number
+            //I opted for using decimal just in case.
+            //I also test for the presence of both numbers.
+            //This code can be largely reused for the other three operators.
+
+            if (CheckBoxValues())
+            {
+                decimal num1;
+                decimal num2;
+                decimal result;
+                bool num1Test = decimal.TryParse(firstNumber.Text, out num1);
+                bool num2test = decimal.TryParse(secondNumber.Text, out num2);
+                if (num1Test && num2test)
+                {
+                    result = num1 - num2;  //Here's where the operator is used.
+                    Result.Text = result.ToString();
+
+                }
+                else
+                {
+                    Result.Text = "Cannot do arithmetic without two numbers. Check inputs and try again.";
+                }
+
+            }
+        }
+
+        protected void Multiplication_Click(object sender, EventArgs e)
+        {
+            //check if the text values can be converted to number
+            //I opted for using decimal just in case.
+            //I also test for the presence of both numbers.
+            //This code can be largely reused for the other three operators.
+
+            if (CheckBoxValues())
+            {
+                decimal num1;
+                decimal num2;
+                decimal result;
+                bool num1Test = decimal.TryParse(firstNumber.Text, out num1);
+                bool num2test = decimal.TryParse(secondNumber.Text, out num2);
+                if (num1Test && num2test)
+                {
+                    result = num1 * num2;  //Here's where the operator is used.
+                    Result.Text = result.ToString();
+
+                }
+                else
+                {
+                    Result.Text = "Cannot do arithmetic without two numbers. Check inputs and try again.";
+                }
+
+            }
+        }
+
+        protected void Division_Click(object sender, EventArgs e)
+        {
+            //check if the text values can be converted to number
+            //I opted for using decimal just in case.
+            //I also test for the presence of both numbers.
+            //This code can be largely reused for the other three operators.
+
+            if (CheckBoxValues())
+            {
+                decimal num1;
+                decimal num2;
+                decimal result;
+                bool num1Test = decimal.TryParse(firstNumber.Text, out num1);
+                bool num2test = decimal.TryParse(secondNumber.Text, out num2);
+                if (num1Test && num2test)  //First make sure they are numbers
+                {
+                    if (num2 != 0)           //Now make sure we aren't dividing by zero
+
+                    {
+                        result = num1 / num2;  //Here's where the operator is used.
+                        Result.Text = result.ToString();
+                    }
+                    else
+                    {
+                        Result.Text = "Division by zero is not allowed.";
+                    }
+                }
+                else
+                {
+                    Result.Text = "Cannot do arithmetic without two numbers. Check inputs and try again.";
+                }
+                    
+               
+            }
+        }
+    }
+}
